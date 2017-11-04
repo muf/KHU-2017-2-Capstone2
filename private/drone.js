1dc2a2c80195cf83506d9ef730e250e3d8a3801c
Phigaro <fkrlsp2@naver.com>
2017-11-04 12:47:57 +0900

    drone takeoff & land test

--- private/drone.js
+++ private/drone.js
@@ -1,16 +1,24 @@
 function drone (){
     var bebop = require('node-bebop');
-
     var bebop_drone = bebop.createClient();
+   
     this.init = function(){
-        console.log("drone_init")
+        console.log("drone_init & takeoff")
         bebop_drone.connect(function() {
             bebop_drone.takeOff();
-          
-            setTimeout(function() {
-                bebop_drone.land();
-            }, 5000);
-          })
+        })
+    }
+
+    this.takeoff = function(){
+        console.log("drone takeoff")
+        bebop_drone.takeOff();
+    }
+
+    this.land = function(){
+        console.log("drone land")
+        setTimeout(function() {
+            bebop_drone.land();
+        }, 5000);
     }
 }
 
