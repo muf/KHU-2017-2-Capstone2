3ff0fd360b388e89ab5182e70bf5d3eca1b9df49
Junghyun.park <junghyun.park@Junghyunparkui-MacBook-Pro.local>
2017-11-04 12:46:27 +0900

    drone.ejs AND drone script update

---
+++ public/javascripts/drone/controller.js
@@ -0,0 +1,16 @@
+
+function init(){
+    alert("init")
+    
+}
+function connect(){
+    alert("connect")
+    
+}
+function takeoff(){
+    alert("takeoff")
+    
+}
+function land(){
+    alert("land")
+}
